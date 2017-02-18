cbb59a0144b59290772a36498c6395a30c30d063
romklao <rk120879@gmail.com>
2017-02-17 18:37:35 -0800

    Fixed parameter of displaying data

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 
 var FOURSQUARE_BASE_URL = 'https://api.foursquare.com/v2/venues/explore';
 var version = '20170101';
-var CLIENT_ID = 'MY3IL31RJIXONBRRWUZ4W444CAXYRBL2WDJTA4X2RV3WLJBW';
-var CLIENT_SECRET = 'FVVETISC243FKU03APPTF5KX53KMEJAHTKUX0IFAHCQVMIDQ';
+var client_id = 'MY3IL31RJIXONBRRWUZ4W444CAXYRBL2WDJTA4X2RV3WLJBW';
+var client_secret = 'FVVETISC243FKU03APPTF5KX53KMEJAHTKUX0IFAHCQVMIDQ';
+// var google_api_key = 'AIzaSyAqAzetfWusypCLeR3E5Wg5njcfp7a_e8w';
 
-function getDataFromApi(searchTerm, callback) {
+
+function initMap() {
+// Create a map object and specify the DOM element for display.
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      scrollwheel: false,
+      zoom: 8
+    });
+}
+
+function getDataFromApi(city, callback) {
     var settings = {
         url: FOURSQUARE_BASE_URL,
         data: {
-            s: searchTerm,
             r: 'json',
+            near: city,
             query: 'romantic',
-            venuePhotos: '1',
+            // venuePhotos: '1',
             limit: '10',
+            radius: '10000',
             v: version,
-            CLIENT_ID: CLIENT_ID,
-            CLIENT_SECRET: CLIENT_SECRET
+            client_id: client_id,
+            client_secret: client_secret
         },
         dataType: 'JSONP',
         type: 'GET',
@@ -29,10 +41,12 @@ function displaySearchData(data) {
     var resultElement = '';
     if(data.response) {
         console.log('data');
-        data.response.groups.forEach(function(item) {
+        data.response.groups[0].items.forEach(function(item) {
             console.log('getdata')
             resultElement += '<p>' + item.venue.name + '</p>' + 
-                            '<p>' + item.venue.location + '</p>';
+
+                            '<p>' + item.venue.location.address + ',' + item.venue.location.city + '</p>' + '<hr>';
+            console.log('results');
         });
     }
     else {
@@ -51,6 +65,7 @@ function searchSubmit() {
 
 $(function() {
     searchSubmit();
+    initMap();
     console.log('hi')
 });
 
